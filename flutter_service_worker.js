2e237826c9d430c9c51ec3b54207603cacda6b6b
JM REMEUR <jm@remeur.fr>
2021-05-27 21:22:11 +0200

    release build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "4fb9c1fd9dd8babe425a5c7c8321c73b",
+  "main.dart.js": "a406a2f9e7529c09e77afc264ee891e1",
+"version.json": "4fb9c1fd9dd8babe425a5c7c8321c73b",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/macos_ui/fonts/SFProTTF/SFProText-Regular.ttf": "85bd46c1cff02c1d8360cc714b8298fa",
 "assets/packages/storybook_flutter/assets/README.md": "fbaed1ae60d80596d99545ef45d72803",
@@ -11,12 +12,11 @@ const RESOURCES = {
 "assets/AssetManifest.json": "511fb9d8e5d1a2de497c5e55bc0e9607",
 "assets/FontManifest.json": "1fb31f67ffac9997bfca39e5608654e4",
 "assets/NOTICES": "0604f1333742dc590c8f6dc7832eab45",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "1d8d844c56d191c967a951e20a34080f",
-"/": "1d8d844c56d191c967a951e20a34080f",
-"main.dart.js": "a406a2f9e7529c09e77afc264ee891e1",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"index.html": "8a93ce2d048604e8da450969774c8864",
+"/": "8a93ce2d048604e8da450969774c8864",
 "manifest.json": "a6ed63fe9a0395b1960325a4d81b80b8"
 };
 
